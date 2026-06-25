184fb3bb3f7837e854b3c8b6c87eb39a5b191153
Alina <alina@MacBook-Pro-mac.local>
2026-06-25 21:35:31 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+// Метод document.write() дозволяє виводити відразу.
+// можна писати скільки хочеш
+
+//   document.write("Hello");
+
+// методи виведення
+
+//  console.log("Hello");
+// console.info("Hello");
+// console.error("Hello");
+// console.warn("Hello");
+
+// ЗМІННІ
+//методи написання змінних
+
+// let age;
+// age = 20;
+// console.log(age);
+
+// let age = 25;
+// console.log(age);
+
+// task
+
+// let age = 10;
+// console.log(age);
+
+// let num = 1246;
+// console.log(num);
+
+//об*єднання змінних
+
+// let num = 10;
+// console.log("Number : " + num + ";" + " cool ");
+
+// можна змінювати значення змінних
+// var num = 10;
+// num = 20;
+// console.log("Number : " + num + ";" + " cool ");
+
+// математичні дії
+
+//var num1 = 3;
+//var nu2 = 7;
+//var res = num1 - nu2;
+//console.log("Віднімання : " + res);
+
+//var num1 = 6;
+//var num2 = 6;
+//var res = num1 + num2;
+//console.log("Додавання : " + res);
+
+//var i1 = 10;
+//var i2 = 20;
+//var res = i1 + i2;
+//console.log("Додавання : " + res);
+
+//var a = 22;
+//var d = 9;
+//var res = a * d;
+//console.log("* : " + res);
+
+//var f = 33;
+//var j = 23;
+//var res = f - j;
+//console.log("- : " + res);
+
+//var a = 3;
+//var b = 9;
+//console.log("Додавання : " + (a + b));
+
+//var n1 = 10;
+//var n2 = 12;
+//console.log("- : " + (n1 - n2));
+
+//var n3 = 5;
+//var h5 = 6;
+//console.log("- : " + (h5 - n3));
+
+//var n3 = 5;
+//n3 = n3 + 5;
+//console.log("res : " + n3);
+
+//var n3 = 5;
+//n3 += 4;
+//console.log("res : " + n3);
+
+//var num_3 = 5;
+//num_3 + 2;
+//console.log("res : " + num_3);
+
+//var num1 = Number("3");
+//var num3 = Number("3");
+//console.log("res : " + (num1 + num3));
+
+console.log("min : " + Math.max(3, 6, -5, 9, 10, -23, 4, 45, 895));
+
+console.log("max : " + Math.max(4, 6, 65, 864, 965, 694, 7, 97));
+
+console.log("min : " + Math.min(3, 6, 56, 64, 1068, 534, -39, -954, 98));
+
+console.log("max : " + Math.max(-34, 500, 687, 954, 9865, 95, 8, 3));
+
+console.log("min : " + Math.min(3, 9, 1, 56, 486, 34, 85, 69));
